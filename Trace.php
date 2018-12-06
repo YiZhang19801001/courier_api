536d98c15edd1fc66e6625d2546e3293a79991ee
YiZhang19801001 <31457917+YiZhang19801001@users.noreply.github.com>
2018-12-06 20:07:34 +1100

    kkk

--- Trace.php
+++ Trace.php
@@ -103,12 +103,12 @@ if($validation_res==1)
             $json_obj = json_decode($json_string);
             
 
-            $flag = json_encode($json_obj->kdgsname);
+            
             $response_arr=array(
                 "orderNumber"=> $json_obj->fydh,
-                "resMsg"=>$flag=="{}"?"order not found":"order found",
-                "resCode"=>$flag=="{}"?"1":"0",
-                "TrackingList"=>$flag!="{}"?$Helper->getTrackingListCQCHS($json_obj->Logisticsback,$json_obj->kdgsname):""
+                "resMsg"=>isset($json_obj->Logisticsback)?"order not found":"order found",
+                "resCode"=>isset($json_obj->Logisticsback)?"1":"0",
+                "TrackingList"=>!isset($json_obj->Logisticsback)?$Helper->getTrackingListCQCHS($json_obj->Logisticsback,$json_obj->kdgsname):""
             );
 
             
